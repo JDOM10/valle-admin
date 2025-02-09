ec2f6963023339233b91ef3887ffa048b5e1464f
JuanMosquera1106 <jdmosqueraa@puce.edu.ec>
2025-02-08 19:26:36 -0500

    Data Productos

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,21 @@
-import Navbar from "@/components/navbar"
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Navbar from "@/components/navbar";
 
 const SetupPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/tipo"); // Redirige automáticamente a "Tipo de Producto"
+  }, []);
+
   return (
     <div className="p-4">
-      <Navbar/>
+      <Navbar />
     </div>
-  )
-}
+  );
+};
 
-export default SetupPage
+export default SetupPage;
